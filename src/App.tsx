a079d8bfafa3329924d47f8c8367c1b4fe2d57a8
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-02-09 23:19:54 -0300

    primeiro Node Criado :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import { useCallback } from 'react';
 import ReactFlow, {
-  Background, Controls,
+  Background, Controls, Position, Node
 } from 'reactflow';
+import { Quadrado } from './components/Nodes/qaudrado';
+
 
 import 'reactflow/dist/style.css';
 
 
+const NODE_TYPES = {
+  quadrado: Quadrado
+}
+
+const INITIAL_NODES = [
+  {
+    id: 'quadrado-1',
+    type: 'quadrado',
+    position: {
+      x:200,
+      y:400,
+    },
+    data: {},
+  },
+] satisfies Node[]
 
 function App() {
   return (
     
-      <ReactFlow>
     <div className='w-screen h-screen'>
-      <Background 
-       gap={20}
-       size={2}
-       color="#07303d"
-       />
-      <Controls />
+      <ReactFlow
+      nodeTypes={NODE_TYPES}
+      nodes={INITIAL_NODES}
+      >
+          <Background 
+          gap={20}
+          size={2}
+          color="#07303d"
+          />
+          <Controls />
+      </ReactFlow>
     </div>
-    </ReactFlow>
    
   )
 }
