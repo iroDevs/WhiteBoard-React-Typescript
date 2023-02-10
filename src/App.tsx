4b76692257400a240c91a17350d7d072a205ef22
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-02-09 22:58:59 -0300

    Install Tailwindcss para estilizar , adciona os controles do board

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { useCallback } from 'react';
 import ReactFlow, {
-  Background,
+  Background, Controls,
 } from 'reactflow';
+
+import 'reactflow/dist/style.css';
+
 
 
 function App() {
   return (
-    <ReactFlow>
-    <div style={{ backgroundColor: 'white' ,width: '100vw', height: '100vh'}}>
+    
+      <ReactFlow>
+    <div className='w-screen h-screen'>
       <Background 
        gap={20}
        size={2}
        color="#07303d"
        />
+      <Controls />
     </div>
     </ReactFlow>
+   
   )
 }
 
