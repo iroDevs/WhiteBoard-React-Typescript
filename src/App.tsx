89db4437fbae0959040ff7ecdd59aa39a2d42224
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2023-02-16 19:38:12 -0300

    colocando a toolbar para funcionar e adcinonando novos squares

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,7 @@ import WhiteBoard from './WhiteBoard';
 function App() {
  return (
   <div className='main'>
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5 text-center ">
-       Bem vindo Ao projeto White Board 
-      </p>
-    </div>
-    <p className='text-2xl text-gray-700 font-bold mb-5 text-center'>
-      O objetivo deste projeto é criar uma plataforma interativa onde os usuários possam desenhar e conectar diferentes elementos (nodos) na tela, como quadrados. O componente ReactFlow está sendo usado para gerenciar o estado e a lógica de conexão entre os nodos, enquanto Taiwind e PostCSS estão sendo usados para garantir uma experiência de usuário elegante e responsiva. Este projeto é uma solução interessante e eficiente para várias necessidades, como brainstorming, apresentações e diagramação.
-    </p>
+    
     <div className='container mx-auto border p-8 m-10' >
       <WhiteBoard />
     </div>
--- src/WhiteBoard.tsx
+++ src/WhiteBoard.tsx
@@ -4,6 +4,7 @@ import ReactFlow, {
 } from 'reactflow';
 import { Quadrado } from './components/Nodes/quadrado';
 import DefaultEdge from './components/Edge/DefaultEdge';
+import * as Toolbar from '@radix-ui/react-toolbar';
 
 import 'reactflow/dist/style.css';
 
@@ -48,9 +49,24 @@ function WhiteBoard() {
   const onConnect = useCallback((connection: Connection)=> {
     return setEdges(edges => addEdge(connection,edges))
   },[])
+
+  function addSquare(){
+    setNodes(nodes =>[
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'quadrado',
+        position: {
+          x:750,
+          y:600,
+        },
+        data: {},
+      }
+    ])
+  }
   return (
     
-    <div className='w-[100%] h-[600px]'>
+    <div className='box w-[100%] h-[650px]'>
       <ReactFlow
       nodeTypes={NODE_TYPES}
       edgeTypes={EDGE_TYPES}
@@ -71,6 +87,12 @@ function WhiteBoard() {
           />
           <Controls />
       </ReactFlow>
+
+      <Toolbar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-400 px-10 h-20 w-90 overflow-hidden">
+        <Toolbar.Button onClick={addSquare} className='h-20 w-20 bg-blue-500 transition-transform hover:-translate-y-2'>
+          ADD
+        </Toolbar.Button>
+      </Toolbar.Root>
     </div>
    
   )
